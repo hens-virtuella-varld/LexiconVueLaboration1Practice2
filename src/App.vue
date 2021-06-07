e7ac6e8089291f5b3aa1c3c6eb23d2305ad46fad
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2021-06-07 14:45:25 +0200

    [main] create Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <Navbar></Navbar>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
     components: {
         Navbar,
+        "app-footer": Footer,
     },
     data() {
         return {};
---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template
+    ><footer class="text-center text-lg-start bg-danger fixed-bottom">
+        <!-- Copyright -->
+        <div class="text-center p-3">
+            © 2021 Copyright:
+            <a class="text-blue" href="https://www.lexicon.se/">Lexicon.se</a>
+        </div>
+        <!-- Copyright -->
+    </footer></template
+>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
